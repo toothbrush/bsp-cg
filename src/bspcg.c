120b92e11f3232cbf2476d6d5d8da135ec645ec4
Paul van der Walt <paul@denknerd.org>
2011-04-01 19:39:59 +0100

    pseudocode CG

--- src/bspcg.c
+++ src/bspcg.c
@@ -6,6 +6,9 @@
 #include "bspcg.h"
 #include "paullib.h"
 
+#define EPS (1.0E-12)
+#define KMAX (100)
+
 /* 
  * Author: Paul van der Walt
  * March 2011
@@ -30,6 +33,45 @@ void bspInitCG(){
     time0=bsp_time();
 
     // begin work
+
+    // input:  A: sparse n*n matrix
+    //         b: dense vector, length n
+    // output: x: dense vector, length n; A.x = b
+
+    double *x = vecallocd(n);
+    int i, k; 
+    for(i = 0; i < n; i++)
+    {
+        x[i] = 0.0; // our best guess.
+    }
+
+    k = 0; // iteration number
+    double* r = vecallocd(n);
+    r = axpy(A,x,minus(b));
+    r = minus(r);
+    double sqrtrho = norm(r); // the remaining residue
+    double rho = sqrtrho*sqrtrho;
+
+    while(sqrtrho > EPS * norm(b) && k < KMAX) {
+        if(k == 0) {
+            p = r;
+        } else {
+            beta = rho/rhoold;
+            p = add(r,beta * p); //TODO axpy here.
+        }
+
+        w = axpy(A,p,0);
+        gamma = vecmul(transpose(p),w);
+        alpha = rho / gamma;
+        x = add(x, alpha * p);
+        r = add(r, -alpha * w);
+        rhoold = rho;
+        sqrtrho = norm(r);
+        rho = sqrtrho * sqrtrho;
+        k++;
+    }
+
+    // here x should be computed. 
 
     // end work
     bsp_sync();  
