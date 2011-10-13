7b8c1acbbe558308c8c7df74e39877ca38022e4d
Paul van der Walt <paul@denknerd.org>
2011-10-13 22:46:02 +0200

    we can generate nonzeroes and filter dupes

--- src/genmat.c
+++ src/genmat.c
@@ -1,6 +1,12 @@
 #include "libs/vecalloc-seq.h"
 #include <stdio.h>
+#include <time.h>
+#include "genmat.h"
 
+#define true 1
+#define false 0
+
+typedef int bool;
 
 int main (int argc, char** argv) {
 
@@ -13,24 +19,70 @@ int main (int argc, char** argv) {
     double mu;
     mu = 2.0; //factor for making matrix diagonal-dominant
 
-    int nz = sparsity*N;
+    int nz = sparsity*N*N;
     int* xs;
     int* ys;
     double* vals;
 
-    printf("We'll now generate a matrix for you.\n");
+    printf("Generating matrix. N=%d, density=%lf, target nz=%d\n", 
+            N, sparsity, nz);
+    srand((unsigned)time(NULL));
+
 
     xs = vecalloci(nz);
     ys = vecalloci(nz);
     vals = vecallocd(nz);
 
+    int v;
+
+
+    for (v=0; v<nz; v++) {
+
+        xs[v]= (double)N * ran();
+        ys[v]= (double)N * ran();
+        vals[v]= ran()*2-1; // [-1,1]
+
+        printf("generated A[%d][%d]=%lf\n", xs[v],ys[v], vals[v]);
+
+    }
+
+
+    // oh my fucking god, look away, it's evil
+
+    int uniques=0;
+    bool found = false;
+
+    int i;
+    for(v=0;v<nz;v++) {
+        // loop through the array, and for each element, check if the preceding 
+        // portion of the array contains the same (i,j) pair. count first occurrences
+        // of (i,j)'s.
+
+        found=false;
+        for(i=0;i<v;i++) {
+            if(xs[v] == xs[i] &&
+               ys[v] == ys[i]) {
+                found = true;
+                //break
+            }
+        }
+        if(!found) {
+            uniques ++;
+
+        }
+
+    }
+
+
+    // TODO guarantee unicity of (i,j)'s
 
 
 
-    
 
 
 
+
+    printf("Generated %d nonzeroes\n", uniques);
 
 
 
@@ -39,4 +91,11 @@ int main (int argc, char** argv) {
     free(vals);
 
     return 0;
+}
+
+
+double ran() {
+
+    return ((double)rand()/(double)RAND_MAX);
+
 }
