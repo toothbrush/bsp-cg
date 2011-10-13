aded7bf975a286ce257ca56651dfa919c44bbfc9
Paul van der Walt <paul@denknerd.org>
2011-10-13 18:53:25 +0200

    slightly more matrix generation crap.

--- src/genmat.c
+++ src/genmat.c
@@ -1,6 +1,42 @@
+#include "libs/vecalloc-seq.h"
 #include <stdio.h>
 
-void main (int argc, char** argv) {
-    printf ( "hi\n");
 
+int main (int argc, char** argv) {
+
+    double sparsity;
+    sparsity = 0.2; // nz = 20% of the size of the matrix
+
+    int N;
+    N = 10; // size of matrix.
+
+    double mu;
+    mu = 2.0; //factor for making matrix diagonal-dominant
+
+    int nz = sparsity*N;
+    int* xs;
+    int* ys;
+    double* vals;
+
+    printf("We'll now generate a matrix for you.\n");
+
+    xs = vecalloci(nz);
+    ys = vecalloci(nz);
+    vals = vecallocd(nz);
+
+
+
+
+    
+
+
+
+
+
+
+    free(xs);
+    free(ys);
+    free(vals);
+
+    return 0;
 }
---
+++ src/libs/vecalloc-seq.h
@@ -0,0 +1,33 @@
+/*
+  ###########################################################################
+  ##      BSPedupack Version 1.0                                           ##
+  ##      Copyright (C) 2004 Rob H. Bisseling                              ##
+  ##                                                                       ##
+  ##      BSPedupack is released under the GNU GENERAL PUBLIC LICENSE      ##
+  ##      Version 2, June 1991 (given in the file LICENSE)                 ##
+  ##                                                                       ##
+  ###########################################################################
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define SZDBL (sizeof(double))
+#define SZINT (sizeof(int))
+#define TRUE (1)
+#define FALSE (0)
+#define MAX(a,b) ((a)>(b) ? (a) : (b))
+#define MIN(a,b) ((a)<(b) ? (a) : (b))
+
+#define SZULL (sizeof( long long))
+#define ulong  long long
+
+double *vecallocd(int n);
+int *vecalloci(int n);
+ulong *vecalloculi(ulong n);
+double **matallocd(int m, int n);
+void vecfreed(double *pd);
+void vecfreeuli(ulong *pd);
+void vecfreei(int *pi);
+void matfreed(double **ppd);
