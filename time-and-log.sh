57ad7848bb6544d0bce68b0d94fc06770a39555f
Paul van der Walt <paul@denknerd.org>
2012-01-13 00:59:59 +0100

    dubious utility to log things

---
+++ time-and-log.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+CMD="$@"
+LOGNAME=`date +'%s'`
+LOGNAME="$LOGNAME-$$"
+
+# redirect all output following this line to logfiles.
+
+exec 1> $LOGNAME.out.log 2>$LOGNAME.err.log
+echo $CMD
+sh -c "$CMD"
