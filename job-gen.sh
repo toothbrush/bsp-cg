fa89077a71b60f96229a72e432576f0997d1f18e
Student v. R. Bisseling <bissstud@p6012.huygens.sara.nl>
2012-01-13 14:54:13 +0100

    generate jobscripts

---
+++ job-gen.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+for i in mats/*.emm ; do
+
+for p in 1 2 4 8 ; do
+
+mat="$i-P$p"
+vec="$i-v$p"
+
+jobname=`basename $mat`.job
+
+tee $jobname > /dev/null <<HERE
+# @ node = 1
+#
+# @ tasks_per_node = $p
+# @ notification = never
+# @ input = /dev/null
+# @ output = out-cg-$mat.\$(jobid)
+# @ error = err-cg-$mat.\$(jobid)
+# @ wall_clock_limit = 00:30:00
+# @ job_type = parallel
+#
+# @ queue
+#
+# @ node_usage = shared
+cd \$HOME/Students10/pdwalt/bsp-cg/
+./src/cg $mat $vec
+HERE
+
+
+done
+done
