466462b82322275d9f7f6733c01e83fdc37e0080
Paul van der Walt <paul@denknerd.org>
2012-01-13 14:12:29 +0100

    more sensible matrix sizes, and easier-to-grep output

--- gen-mats.sh
+++ gen-mats.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
+./time-and-log.sh ./src/genmat 1000  150 0.01 &
+./time-and-log.sh ./src/genmat 1000  15 0.001 &
+./time-and-log.sh ./src/genmat 1000  75  0.005 &
+./time-and-log.sh ./src/genmat 2000  150 0.01 &
+./time-and-log.sh ./src/genmat 2000  15 0.001 &
+./time-and-log.sh ./src/genmat 2000  75  0.005 &
 ./time-and-log.sh ./src/genmat 5000  150 0.01 &
 ./time-and-log.sh ./src/genmat 5000  15 0.001 &
 ./time-and-log.sh ./src/genmat 5000  75  0.005 &
 ./time-and-log.sh ./src/genmat 10000 300 0.01 &
 ./time-and-log.sh ./src/genmat 10000 30 0.001 &
 ./time-and-log.sh ./src/genmat 10000 150 0.005 &
-./time-and-log.sh ./src/genmat 40000 1200 0.01 &
-./time-and-log.sh ./src/genmat 40000 120 0.001 &
-./time-and-log.sh ./src/genmat 40000 600 0.005 &
-./time-and-log.sh ./src/genmat 80000 2400 0.01 &
-./time-and-log.sh ./src/genmat 80000 240 0.001 &
-./time-and-log.sh ./src/genmat 80000 1200 0.005 &
+./time-and-log.sh ./src/genmat 20000 1200 0.01 &
+./time-and-log.sh ./src/genmat 20000 120 0.001 &
+./time-and-log.sh ./src/genmat 20000 600 0.005 &
